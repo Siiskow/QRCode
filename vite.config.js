7cfbcf35d0e5d4f2c02f7a9b32547ee0e7a094be
Siiskow <siiskow21@gmail.com>
2022-10-24 21:04:54 +0100

    setting for netlify

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,17 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  kit :{
+    target : '#svelte',
+    adapter: adapter(),
+    prerender : {
+      crawl : true,
+      enabled:true,
+      onError:'continue',
+      pages:['*'],
+    }
+  }
 })
 
 
